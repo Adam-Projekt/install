0fae15fb7cbb949570636fa0f62f09140036c583
Adam MegaRules <adammegarules1@gmail.com>
2026-03-31 22:26:09 +0200

    upgrading script

--- install.bash
+++ install.bash
@@ -32,44 +32,44 @@ start() {
 
 # curl -f https://zed.dev/install.sh | sh
 flatpack_install() {
-    flatpak install flathub com.bitwarden.desktop
-    flatpak install -y flathub org.prismlauncher.PrismLauncher
+  flatpak install flathub com.bitwarden.desktop
+  flatpak install -y flathub org.prismlauncher.PrismLauncher
 }
 rust_install() {
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-    source "$HOME/.cargo/env"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source "$HOME/.cargo/env"
 }
 bun_install() {
-    curl -fsSL https://bun.sh/install | bash
+  curl -fsSL https://bun.sh/install | bash
 }
 ghostty_install() {
-    sudo dnf copr enable scottames/ghostty -y
-    sudo dnf install -y ghostty
+  sudo dnf copr enable scottames/ghostty -y
+  sudo dnf install -y ghostty
 }
 firefox_defedora() {
-    sudo rm -f /usr/lib64/firefox/browser/defaults/preferences/firefox-redhat-default-prefs.js
+  sudo rm -f /usr/lib64/firefox/browser/defaults/preferences/firefox-redhat-default-prefs.js
 }
 nvim_install() {
-    mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)" 2>/dev/null || true
-    rm -rf "$HOME/.config/nvim"
-    git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
-    rm -rf "$HOME/.config/nvim/.git"
+  mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)" 2>/dev/null || true
+  rm -rf "$HOME/.config/nvim"
+  git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+  rm -rf "$HOME/.config/nvim/.git"
 }
 fish_install() {
-    # Přidá jen "fish" do .bashrc (pokud tam ještě není)
-    if ! grep -q "^fish" ~/.bashrc; then
-        echo "fish" >> ~/.bashrc
-    fi
+  # Přidá jen "fish" do .bashrc (pokud tam ještě není)
+  if ! grep -q "^fish" ~/.bashrc; then
+    echo "fish" >>~/.bashrc
+  fi
 
-    fish -c "set -U fish_greeting"
+  fish -c "set -U fish_greeting"
 }
 docker_install() {
-    curl -fsSL https://get.docker.com | bash
+  curl -fsSL https://get.docker.com | bash
 }
 sublime_install() {
-    sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-    sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-    sudo dnf install sublime-text -y
+  sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+  sudo dnf config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+  sudo dnf install sublime-text -y
 }
 
 sudo dnf upgrade -y
@@ -82,4 +82,4 @@ bun_install
 fish_install
 flatpack_install
 docker_install
-# sublime_install
+sublime_install
