d541bf5d45d2386f2342e93cea4f798b530d1101
Adam MegaRules <adammegarules1@gmail.com>
2026-03-31 19:20:28 +0200

    upgrading script

--- install.bash
+++ install.bash
@@ -9,15 +9,16 @@ packages=(
   treesitter
   lua
   git
+  libreoffice
+  gnome-boxes
+  fish
 )
 
-# ====== default install ======
 install() {
   pkg="$1"
   sudo dnf install -y "$pkg"
 }
 
-# ====== loop ======
 start() {
   for pkg in "${packages[@]}"; do
     echo "Installing $pkg..."
@@ -25,35 +26,49 @@ start() {
   done
 }
 
-special() {
+# curl -f https://zed.dev/install.sh | sh
+#curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh
+
+flatpack_install() {
+    flatpak install flathub com.bitwarden.desktop
+    flatpak install -y flathub org.prismlauncher.PrismLauncher
+}
+rust_install() {
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    source "$HOME/.cargo/env"
+}
+bun_install() {
     curl -fsSL https://bun.sh/install | bash
+}
+ghostty_install() {
+    sudo dnf copr enable scottames/ghostty -y
+    sudo dnf install -y ghostty
+}
+firefox_defedora() {
     sudo rm -f /usr/lib64/firefox/browser/defaults/preferences/firefox-redhat-default-prefs.js
-    # ghostty
-  sudo dnf copr enable scottames/ghostty -y
-  sudo dnf install -y ghostty
+}
+nvim_install() {
+    mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)" 2>/dev/null || true
+    rm -rf "$HOME/.config/nvim"
+    git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
+    rm -rf "$HOME/.config/nvim/.git"
+}
+fish_install() {
+    # Přidá jen "exec fish" do .bashrc (pokud tam ještě není)
+    if ! grep -q "^exec fish" ~/.bashrc; then
+        echo "exec fish" >> ~/.bashrc
+    fi
 
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-  source "$HOME/.cargo/env"
-
-  # curl -f https://zed.dev/install.sh | sh
-
-  sudo dnf install -y gnome-boxes
-
-  sudo dnf install -y libreoffice
-
-  flatpak install -y flathub org.prismlauncher.PrismLauncher
-
-  #curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh && rm get-docker.sh
-  mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak.$(date +%s)" 2>/dev/null || true
-  rm -rf "$HOME/.config/nvim"
-  git clone https://github.com/LazyVim/starter "$HOME/.config/nvim"
-  rm -rf "$HOME/.config/nvim/.git"
-
-  flatpak install flathub com.bitwarden.desktop
-
-
+    fish -c "set -U fish_greeting"
 }
 
 sudo dnf upgrade -y
-#start
+start
 special
+rust_install
+nvim_install
+firefox_defedora
+ghostty_install
+bun_install
+fish_install
+flatpack_install
